a1a7d755a7a3d3abfebe7b5cfacdb499fb7dc2e8
Mohdanz <mohddandii@gmail.com>
2026-01-11 00:35:32 +0700

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,33 +1,15 @@
-const popup = document.getElementById("popup");
-const title = document.getElementById("popupTitle");
-const msg = document.getElementById("popupMsg");
-
-function togglePassword(){
-  const p = document.getElementById("password");
-  p.type = p.type === "password" ? "text" : "password";
-}
-
-function showPopup(t, m){
-  title.innerText = t;
-  msg.innerText = m;
-  popup.classList.add("show");
-}
-
-function closePopup(){
-  popup.classList.remove("show");
-}
-
-document.getElementById("loginForm").addEventListener("submit", function(e){
+loginForm.onsubmit = e => {
   e.preventDefault();
-
   const u = username.value;
   const p = password.value;
 
-  if(u === "admin" && p === "1234"){
-    localStorage.setItem("login","true");
-    showPopup("Login Berhasil", "Selamat datang di sistem MD CELL");
-    // window.location.href = "kasir.html";
+  if(u==="admin" && p==="admin123"){
+    localStorage.setItem("role","admin");
+    location.href="admin.html";
+  } else if(u==="kasir" && p==="kasir123"){
+    localStorage.setItem("role","kasir");
+    location.href="kasir.html";
   } else {
-    showPopup("Login Gagal", "Username atau password salah");
+    alert("Login salah");
   }
-});
+};
