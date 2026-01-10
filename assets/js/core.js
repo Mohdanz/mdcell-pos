b2b07e419d12c5cfc9798e985d44d1d6d0507c56
Mohdanz <mohddandii@gmail.com>
2026-01-11 02:27:21 +0700

    Update core.js

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,1 +1,26 @@
+// CORE STORAGE
+const DB = {
+  get(k){ return JSON.parse(localStorage.getItem(k)); },
+  set(k,v){ localStorage.setItem(k, JSON.stringify(v)); }
+};
 
+// INIT SYSTEM (JANGAN HAPUS)
+(function init(){
+  if(!DB.get("auth")) DB.set("auth",{ role:null });
+
+  if(!DB.get("state")){
+    DB.set("state",{
+      shiftStatus:"closed",   // open | closed
+      shiftId:0,
+      saldoAwal:0,
+      uangLaci:0,
+      income:0
+    });
+  }
+
+  if(!DB.get("transactions")) DB.set("transactions",[]);
+  if(!DB.get("archives")) DB.set("archives",[]);
+
+  if(!DB.get("ewallets")) DB.set("ewallets",[]);
+  if(!DB.get("items")) DB.set("items",[]);
+})();
