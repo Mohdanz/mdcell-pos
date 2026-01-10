866adada46fef6fe3d2f4cda4d346784b8634cc3
Mohdanz <mohddandii@gmail.com>
2026-01-11 00:03:08 +0700

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,14 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
+document.getElementById("loginForm").addEventListener("submit", function(e){
   e.preventDefault();
-  alert("Login berhasil (demo)");
+
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+
+  if(user === "admin" && pass === "1234"){
+    localStorage.setItem("login","true");
+    alert("Login berhasil");
+    // window.location.href = "kasir.html";
+  } else {
+    alert("Username atau password salah");
+  }
 });
