fef030ffe09ca958d9756addab95916d3cdc62ab
Mohdanz <mohddandii@gmail.com>
2026-01-10 23:33:50 +0700

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,4 @@
-const toggle = document.getElementById('togglePassword');
-const password = document.getElementById('password');
-
-toggle.onclick = () => {
-  password.type = password.type === 'password' ? 'text' : 'password';
-};
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Login berhasil (demo)");
+});
