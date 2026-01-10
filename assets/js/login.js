e03f11c7a251fe2318482a80016c2687d14b3268
Mohdanz <mohddandii@gmail.com>
2026-01-10 22:37:36 +0700

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,41 +1,6 @@
-const form = document.getElementById('loginForm');
-const btn = document.getElementById('loginBtn');
-const error = document.getElementById('errorMsg');
 const toggle = document.getElementById('togglePassword');
 const password = document.getElementById('password');
 
 toggle.onclick = () => {
   password.type = password.type === 'password' ? 'text' : 'password';
 };
-
-form.onsubmit = e => {
-  e.preventDefault();
-
-  error.textContent = '';
-  btn.classList.add('loading');
-  btn.textContent = 'Memproses...';
-
-  const user = username.value.trim();
-  const pass = password.value.trim();
-
-  setTimeout(() => {
-    if (!user || !pass) {
-      error.textContent = 'Username dan password wajib diisi';
-      resetBtn();
-      return;
-    }
-
-    // SIMULASI LOGIN
-    if (user === 'admin' && pass === 'admin') {
-      alert('Login berhasil');
-    } else {
-      error.textContent = 'Username atau password salah';
-      resetBtn();
-    }
-  }, 1200);
-};
-
-function resetBtn() {
-  btn.classList.remove('loading');
-  btn.textContent = '⚡ Masuk ke Sistem';
-}
