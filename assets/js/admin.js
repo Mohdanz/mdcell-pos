d22875b3c62b3fc9b43eaa250c0f5ec128041284
Mohdanz <mohddandii@gmail.com>
2026-01-11 00:40:43 +0700

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,6 @@
-if(localStorage.getItem("role")!=="admin") location.href="login.html";
+if(localStorage.getItem("role")!=="admin"){
+  location.href="login.html";
+}
 
 const total = Number(localStorage.getItem("grandTotal")) || 0;
 document.getElementById("total").innerText = "Rp " + total;
