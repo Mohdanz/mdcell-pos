dd3cc2216b10100cd3f0e81da4a44a91a7b44ae2
Mohdanz <mohddandii@gmail.com>
2026-01-11 00:36:17 +0700

    Update kasir.js

--- assets/js/kasir.js
+++ assets/js/kasir.js
@@ -1,1 +1,21 @@
+if(localStorage.getItem("role")!=="kasir") location.href="login.html";
 
+let total = Number(localStorage.getItem("kasirTotal")) || 0;
+document.getElementById("total").innerText = total;
+
+function tambah(){
+  const h = Number(harga.value);
+  total += h;
+  localStorage.setItem("kasirTotal", total);
+
+  const g = Number(localStorage.getItem("grandTotal")) || 0;
+  localStorage.setItem("grandTotal", g + h);
+
+  document.getElementById("total").innerText = total;
+  harga.value="";
+}
+
+function logout(){
+  localStorage.clear();
+  location.href="login.html";
+}
