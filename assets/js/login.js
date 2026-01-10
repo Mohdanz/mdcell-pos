6653cd0242e12abb8d5dcfa0c02572419e718aec
Mohdanz <mohddandii@gmail.com>
2026-01-11 00:08:06 +0700

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,14 +1,33 @@
+const popup = document.getElementById("popup");
+const title = document.getElementById("popupTitle");
+const msg = document.getElementById("popupMsg");
+
+function togglePassword(){
+  const p = document.getElementById("password");
+  p.type = p.type === "password" ? "text" : "password";
+}
+
+function showPopup(t, m){
+  title.innerText = t;
+  msg.innerText = m;
+  popup.classList.add("show");
+}
+
+function closePopup(){
+  popup.classList.remove("show");
+}
+
 document.getElementById("loginForm").addEventListener("submit", function(e){
   e.preventDefault();
 
-  const user = document.getElementById("username").value;
-  const pass = document.getElementById("password").value;
+  const u = username.value;
+  const p = password.value;
 
-  if(user === "admin" && pass === "1234"){
+  if(u === "admin" && p === "1234"){
     localStorage.setItem("login","true");
-    alert("Login berhasil");
-    // window.location.href = "kasir.html";
+    showPopup("Login Berhasil", "Selamat datang di sistem MD CELL");
+    // window.location.href="kasir.html";
   } else {
-    alert("Username atau password salah");
+    showPopup("Login Gagal", "Username atau password salah");
   }
 });
